e9b21032c23172f16a093358a6f66b7f5128668f
sebsolezzi88 <ssolezzi1@gmail.com>
2025-06-20 10:32:54 -0300

    Agregando propiedades a la interfaz

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,11 +4,15 @@ export const mostrarRegistro = (req: Request, res: Response) => {
   return res.render("registro");
 };
 
-export const submitRegistro = (req: Request, res: Response) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const passwordr = req.body.passwordr;
 
-    console.log(username, password,passwordr);
-    return res.send('lis');
+export const submitRegistro = (req: Request, res: Response) => {
+    const username = req.body.username.trim();
+    const password = req.body.password.trim();
+    const passwordr = req.body.passwordr.trim();
+
+    if(username ==='' || password ==='' || passwordr ===''){
+      req.session.error = 'Debe completar todos los campos';
+      res.redirect('/registro');
+    }
+    return res.send('listo');
 };
